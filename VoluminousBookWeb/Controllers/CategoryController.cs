20e1cb27ad46f59a595afc35d2433a2090e6ad02
Shreekrisna <shree.kris810@gmail.com>
2023-11-29 20:16:22 +0545

    firstmvcproj

---
+++ VoluminousBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VoluminousBookWeb.Data;
+using VoluminousBookWeb.Models;
+
+namespace VoluminousBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //folder 2 video 8
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objcategoryList = _db.Categories;
+            return View(objcategoryList);
+        }
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //help and prevent from crosssite request forgery attack
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DiplayOrder cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);   
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken] //help and prevent from crosssite request forgery attack
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DiplayOrder cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken] //help and prevent from crosssite request forgery attack
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            
+            
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+            
+            
+        }
+    }
+}
---
+++ VoluminousBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VoluminousBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,500,ErrorMessage ="Order must be between 1 and 500")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+    }
+}
